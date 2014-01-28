a39ab0ecbe27b998bf68d3984bf3ec1b43afebb0
Nestor <nmaletm@gmail.com>
2014-01-28 15:24:01 +0100

    Estructura inicial

---
+++ AppMediterrania/AppMediterrania/Controllers/MapViewController.h
@@ -0,0 +1,15 @@
+//
+//  MapViewController.h
+//  AppMediterrania
+//
+//  Created by Nestor Malet on 28/01/14.
+//  Copyright (c) 2014 Storn.es. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+- (IBAction)showInformation:(id)sender;
+
+@end
---
+++ AppMediterrania/AppMediterrania/Controllers/MapViewController.m
@@ -0,0 +1,44 @@
+//
+//  MapViewController.m
+//  AppMediterrania
+//
+//  Created by Nestor Malet on 28/01/14.
+//  Copyright (c) 2014 Storn.es. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+@interface MapViewController ()
+
+@end
+
+@implementation MapViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark Actions
+
+- (IBAction)showInformation:(id)sender{
+    
+}
+
+@end
